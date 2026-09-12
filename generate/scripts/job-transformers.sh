1a1a2d9b16b179cbe1ddbffc7bd7c418ad091084
Maxime Boucher <maxime@maximeboucher.com>
2026-09-12 09:10:59 -0600

    Increase transformers generate job time allocation

--- generate/scripts/job-transformers.sh
+++ generate/scripts/job-transformers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=generate-comments
-#SBATCH --time=00:45:00
+#SBATCH --time=03:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
